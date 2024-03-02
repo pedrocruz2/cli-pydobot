648768e429fab7d78a178741e200b4b611fe63f6
root <root@asusxd>
2024-03-01 23:48:09 -0300

    menus flet

--- graphicinterface.py
+++ graphicinterface.py
@@ -2,8 +2,7 @@ import flet as ft
 from cli.classes.robo import Robo 
 from serial.tools import list_ports
 
-robo = Robo()
-def command_handler(command):
+def command_handler(command,page: ft.Page):
     match command:
         case "home":
             # Lógica para enviar o robô para a posição inicial
@@ -25,12 +24,22 @@ def command_handler(command):
             print("Fechando aplicação")
         case _:
             print("Comando não reconhecido")
-
 def main(page: ft.Page):
     page.title = "Controle do Robô PyDobot"
     page.vertical_alignment = ft.MainAxisAlignment.START
+    def on_port_selected(e):
+        global porta_escolhida
+        porta_escolhida = e.control.value
+        print(f"Porta escolhida: {porta_escolhida}")
+    available_ports = list_ports.comports()
+    port_options = [ft.dropdown.Option(port.device, port.device) for port in available_ports]
 
-    # Criação do menu dropdown
+    # Criação do menu dropdown para seleção de portas
+    port_dropdown = ft.Dropdown(
+        label="Escolha a porta serial",
+        options=port_options,
+        on_change=on_port_selected
+    )
     dropdown = ft.Dropdown(
         label="Selecione um comando",
         options=[
@@ -41,10 +50,11 @@ def main(page: ft.Page):
             ft.dropdown.Option("atual", "Atual"),
             ft.dropdown.Option("sair", "Sair")
         ],
-        on_change=lambda e: command_handler(e.control.value),
+        on_change=lambda e: command_handler(e.control.value,page),
     )
-
+    page.add(port_dropdown)
     page.add(dropdown)
+    
 
 if __name__ == "__main__":
     ft.app(target=main)
